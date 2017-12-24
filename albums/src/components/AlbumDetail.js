32f078abc2080d7bba33f895a8a5864044106058
Yilong <yhua4993@uni.sydney.edu.au>
2017-12-24 23:16:21 +1100

    - initiation and basic layout

---
+++ albums/src/components/AlbumDetail.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {
+  Text,
+  View,
+  Image,
+} from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+
+const AlbumDetail = ({ album }) => {
+  const { 
+    title,
+    artist,
+    thumbnail_image,
+    image,
+  } = album;
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle,
+  } = styles;
+
+  return (
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            source={{ uri: thumbnail_image }}
+            style={thumbnailStyle}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>
+            {title}
+          </Text>
+          <Text>
+            {artist}
+          </Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          source={{ uri: image }}
+          style={imageStyle}
+        />
+      </CardSection>
+    </Card>
+  );
+};
+
+const styles = {
+  headerContentStyle: {
+    justifyContent: 'space-around',
+    flexDirection: 'column',
+  },
+  thumbnailStyle: {
+    width: 50,
+    height: 50,
+  },
+  headerTextStyle: {
+    fontSize: 18,
+  },
+  // a trick to stretch image
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null,
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+};
+
+export default AlbumDetail;
---
+++ albums/src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const styles = {
+  textStyle: {
+    fontSize: 20,
+  },
+  viewStyle: {
+    justifyContent: 'center',
+    paddingTop: 10,
+    alignItems: 'center',
+    backgroundColor: '#42A5F5',
+    height: 60,
+    shadowColor: '#333',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+  },
+};
+
+const Header = (props) => {
+  const { viewStyle, textStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>
+        {props.headerText}
+      </Text>
+    </View>
+  );
+};
+
+export default Header;
