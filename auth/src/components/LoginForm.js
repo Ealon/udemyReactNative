46bc6cb28e5e9bf075578819a6def0eca44bd830
Yilong <yhua4993@uni.sydney.edu.au>
2017-12-25 17:59:48 +1100

    login form layout complete

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
 import {
-  // Text,
-  View
+  TextInput,
 } from 'react-native';
 import {
   Button,
   Card,
   CardSection,
+  Input,
 } from './common';
 
 class LoginForm extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+  
   render() {
     return (
       <Card>
-        <CardSection></CardSection>
-        <CardSection></CardSection>
+        <CardSection>
+          <Input
+            label="Email"
+            value = {this.state.email}
+            onChangeText={email => this.setState({ email })}
+            placeholder="user@gmail.com"
+          />
+        </CardSection>
+        
+        <CardSection>
+          <Input
+            secureTextEntry
+            label="Password"
+            value = {this.state.password}
+            onChangeText={password => this.setState({ password })}
+            placeholder="password"
+          />
+        </CardSection>
+        
         <CardSection>
           <Button>log in</Button>
         </CardSection>
